7e04741b8379af5f49bb3ad8a51a7e18b1504b47
Stephan Sommer <steven.sommer@go.winona.edu>
2024-01-21 18:13:31 -0600

    Update DrawPanel.java

--- DrawPanel.java
+++ DrawPanel.java
@@ -2,6 +2,7 @@ package playball;
 
 import javax.swing.*;
 import java.awt.*;
+
 class DrawPanel extends JPanel {
 	
     private int oneX = 7;
@@ -10,7 +11,7 @@ class DrawPanel extends JPanel {
     boolean down = true;
     boolean left = false;
     boolean right = true;
-
+    int score = 0;
 
     public void paintComponent(Graphics g) {
         g.setColor(Color.BLUE);
@@ -21,14 +22,11 @@ class DrawPanel extends JPanel {
         g.fillRect(6, 6, this.getWidth()-12, this.getHeight()-12);
         g.setColor(Color.GREEN);
         g.fillOval(oneX, oneY, 16, 16);
+        
 
     }
-   
 
-
-
-    public void moveIt()
-    {
+    public void moveIt() {
 
         if(oneX >= 283){
             right = false;
@@ -59,8 +57,13 @@ class DrawPanel extends JPanel {
             oneX++;
         }
         
+     // Check if the oval hits the edge, and increment the score
+        if (oneX >= 283 || oneX <= 7 || oneY >= 259 || oneY <= 7) {
+            score++;
+            // Update the scoreLabel with the current score
+            Tester.scoreLabel.setText("Score: " + score);
+         }
+          
         repaint();
-
-
     }
  }
